2affdde20f669cf352057fc2aac4b978752d7e80
Coda Hale <coda.hale@gmail.com>
2010-07-23 15:04:22 -0700

    Added before/aferEach/All.

--- src/main/scala/com/codahale/simplespec/Spec.scala
+++ src/main/scala/com/codahale/simplespec/Spec.scala
@@ -8,13 +8,30 @@ abstract class Spec extends Specification {
   {
     val klass = Class.forName(this.getClass.getName.replace("$", ""))
     for (susKlass <- klass.getDeclaredClasses) {
-      NameTransformer.decode(susKlass.getSimpleName) should {
+      NameTransformer.decode(susKlass.getSimpleName) should { beforeAll.before
+        afterAll.after
+        val beforeEachMethod = try {
+          Some(susKlass.getDeclaredMethod("beforeEach"))
+        } catch {
+          case e: Exception => None
+        }
+        val afterEachMethod = try {
+          Some(susKlass.getDeclaredMethod("afterEach"))
+        } catch {
+          case e: Exception => None
+        }
+        
         for (exampleMethod <- susKlass.getDeclaredMethods if exampleMethod.getName.startsWith("should")) {
           val name = NameTransformer.decode(exampleMethod.getName)
           name.substring("should".length) >> {
             val instance = susKlass.getConstructor().newInstance()
             try {
-              exampleMethod.invoke(instance)
+              beforeEachMethod.map { _.invoke(instance) }
+              try {
+                exampleMethod.invoke(instance)
+              } finally {
+                afterEachMethod.map { _.invoke(instance) }
+              }
             } catch {
               case e: InvocationTargetException =>
                 throw e.getCause
@@ -23,5 +40,13 @@ abstract class Spec extends Specification {
         }
       }
     }
+  }
+  
+  def beforeAll {
+    // override me for awesome fun
+  }
+  
+  def afterAll {
+    // override me for awesome fun
   }
 }
--- src/test/scala/com/codahale/simplespec/specs/SpecSpec.scala
+++ src/test/scala/com/codahale/simplespec/specs/SpecSpec.scala
@@ -5,9 +5,38 @@ import com.codahale.simplespec.Spec
 // TODO: figure out how to test this monster
 
 object SpecSpec extends Spec {
+  
+  override def beforeAll() {
+    println("before all")
+  }
+  
+  override def afterAll() {
+    println("after all")
+  }
+  
+  class `The simplest thing ever` {
+    def `should work` {
+      
+    }
+  }
+  
   class `A crazy object-oriented spec` {
+    def beforeEach() {
+      println("before each")
+    }
+    
     def `should be awesome` {
+      println("during awesome")
       true must be(true)
+    }
+    
+    def `should be kind to others` {
+      println("during kind")
+      true must be(true)
+    }
+    
+    def afterEach() {
+      println("after each")
     }
   }
 }
